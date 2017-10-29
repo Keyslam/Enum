ccf70ef4aa11945d9090fbcdda32f6d2fa7a9ea9
Justin van der Leij <justinvanderleij@gmail.com>
2017-10-29 23:50:17 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,25 @@
+local Enum = {}
+local Meta = {
+   __index    = function(_, k) error("Attempt to index non-existant enum '"..k.."'.") end,
+   __newindex = function()     error("Attempt to write to static enum") end,
+}
+
+function Enum.new(...)
+   local values = {...}
+
+   if type(values[1]) == "table" then
+      values = values[1]
+   end
+
+   local enum = {}
+
+   for i = 1, #values do
+      enum[values[i]] = values[i]
+   end
+
+   return setmetatable(enum, Meta)
+end
+
+return setmetatable(Enum, {
+   __call  = function(_, ...) return Enum.new(...) end,
+})
